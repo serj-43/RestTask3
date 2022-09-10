a503f1703a9ad6ea1eb592800b02e29d79738a65
serj-43 <serj-43@yandex.ru>
2022-09-10 22:20:38 +0300

    initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.List;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPostSomeDate() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some text value")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some text value"))
+        ;
+    }
+}

1c311d2060bb881f0c99de206c640dc58fdf12dd
serj-43 <serj-43@yandex.ru>
2022-09-10 22:30:34 +0300

    error added

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some text value"))
+                .body("data", equalTo("sometextvalue"))
         ;
     }
 }
